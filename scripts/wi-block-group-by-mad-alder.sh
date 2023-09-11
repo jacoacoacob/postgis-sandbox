bbd1b3a66fdde56fca06b50737995936f82937bb
jacoacoacob <jacoacoacob@protonmail.com>
2023-09-11 11:55:36 -0500

    select block groups with alder district area overlap gt threshold

---
+++ scripts/wi-block-group-by-mad-alder.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+docker compose exec -T db psql -v ON_ERROR_STOP=1 -U postgres --no-align --tuples-only <<EOSQL
+    SELECT jsonb_build_object(
+        'type', 'FeatureCollection',
+        'features', json_agg(t.feature)
+    )
+    FROM (
+        SELECT jsonb_build_object(
+            'type', 'Feature',
+            'id', props.gid,
+            'geometry', ST_AsGeoJSON(ST_ReducePrecision(props.geom, 0.0001))::jsonb,
+            'props', to_jsonb(props.*) - 'geom' - 'gid'
+        ) AS feature
+        FROM (
+            SELECT
+                bg.gid::text,
+                bg.geom,
+                bg.namelsad AS name
+            FROM tl_2022_55_bg AS bg
+            INNER JOIN alder_districts AS ad
+            ON (
+                -- Only return rows where alder_district geometry area overlaps 
+                -- a block group area geometry by more than 5 percent
+                ST_Area(ST_Intersection(ad.geom, bg.geom)) / ST_Area(bg.geom) * 100 > 5
+            )
+            GROUP BY bg.gid
+        ) AS props
+    ) AS t
+EOSQL
