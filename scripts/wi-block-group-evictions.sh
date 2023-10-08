726d3378949208ca07cc585365a7b069638912f1
jacoacoacob <jacoacoacob@protonmail.com>
2023-10-08 12:37:24 -0500

    block group evicitons with flat eviciton data model

--- scripts/wi-block-group-evictions.sh
+++ scripts/wi-block-group-evictions.sh
@@ -15,25 +15,39 @@ docker compose exec -T db psql -v ON_ERROR_STOP=1 -U postgres --no-align --tuple
             'properties', to_jsonb(props) - 'geom' - 'id' - 'geoid'
         ) feature
         FROM (
-            SELECT *
-            FROM (
-                SELECT
-                    geoid                                id,
-                    json_agg(to_jsonb(bgec.*) - 'geoid') evictions
-                  FROM (
-                    SELECT
-                        COUNT(*)                           n_filings,
-                        DATE_TRUNC('year', ec.filing_date) filing_year,
-                        bgho.geoid                        
-                      FROM bg_housing_occupancy bgho
-                      JOIN eviction_cases       ec
-                        ON ST_Contains(bgho.geom, ec.defendant_address_point)
-                  GROUP BY filing_year, geoid
-                ) bgec
-              GROUP BY id
-            ) bgec
-            JOIN bg_housing_occupancy bgho
-              ON bgec.id = bgho.geoid
+            SELECT
+                madbg.block_group_name || '_' ||EXTRACT(YEAR FROM madbg.filing_year) id,
+                madbg.block_group_name geog_name,
+                madbg.filing_year,
+                ec.n_filings,
+                case
+                  when ec.n_filings = 0 then 0
+                  when ho.renter_count = 0 then 0
+                  else round(((ec.n_filings::float / ho.renter_count::float) * 100)::numeric, 2)
+                end AS filing_rate,
+                ho.owner_count,
+                ho.renter_count,
+                madbg.geom
+              FROM mad_block_groups madbg
+              JOIN tl_2022_55_bg    tlbg
+                ON madbg.gid = tlbg.gid::text
+              JOIN tl_2022_55_tract tr
+                ON tlbg.countyfp = tr.countyfp AND tlbg.tractce = tr.tractce
+              JOIN housing_census   ho
+                ON ho.tract = tr.name AND tlbg.blkgrpce = ho.block_group::text
+              JOIN (
+                        SELECT
+                            gid,
+                            filing_year,
+                            COUNT(defendant_address) n_filings
+                          FROM mad_block_groups madbg
+               LEFT OUTER JOIN eviction_cases   ec
+                            ON
+                              ST_Contains(madbg.geom, ec.defendant_address_point) AND
+                              madbg.filing_year = DATE_TRUNC('year', ec.filing_date)
+                      GROUP BY gid, filing_year
+              ) ec
+                ON ec.gid = madbg.gid AND ec.filing_year = madbg.filing_year
         ) props
     ) f;
 EOSQL
